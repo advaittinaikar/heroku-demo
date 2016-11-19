f58c9c3947c8aef3f53bb22e92fc200c2cb9c09d
advaittinaikar <advait.tinaikar>
2016-11-19 01:04:57 -0500

    Something commit

--- app.rb
+++ app.rb
@@ -68,143 +68,143 @@ get '/incoming_sms' do
 end
 
 
-get '/tasks', :provides => [:html, :json, :xml] do
+# get '/tasks', :provides => [:html, :json, :xml] do
   
-  #Task.all.to_json
-  @tasks = Task.all
-  #
-  # respond_to do |f|
-  #   f.xml { @tasks.to_xml }
-  #   f.on( 'text/json' ) { @tasks.to_json }
-  #   f.on( 'text/html' ) { "wooops" }
-  #   f.on( 'application/json' ) { @tasks.to_json }
-  #   #f.on('*/*') {  Task.all.map{ |t| t.to_s }.to_s }
-  #   f.on('*/*') { haml :'tasks/index' }
-  # end
+#   #Task.all.to_json
+#   @tasks = Task.all
+#   #
+#   # respond_to do |f|
+#   #   f.xml { @tasks.to_xml }
+#   #   f.on( 'text/json' ) { @tasks.to_json }
+#   #   f.on( 'text/html' ) { "wooops" }
+#   #   f.on( 'application/json' ) { @tasks.to_json }
+#   #   #f.on('*/*') {  Task.all.map{ |t| t.to_s }.to_s }
+#   #   f.on('*/*') { haml :'tasks/index' }
+#   # end
 
-  # respond_to do |f|
-  #     f.xml { @tasks.to_xml }
-  #     f.on( 'text/json' ) { @tasks.to_json }
-  #     #f.on( 'text/html' ) { "wooops" }
-  #     f.on( 'application/json' ) { @tasks.to_json }
-  #     #f.on('*/*') {  Task.all.map{ |t| t.to_s }.to_s }
-  #     f.on('*/*') { haml :'tasks/index' }
-  #
-  # end
+#   # respond_to do |f|
+#   #     f.xml { @tasks.to_xml }
+#   #     f.on( 'text/json' ) { @tasks.to_json }
+#   #     #f.on( 'text/html' ) { "wooops" }
+#   #     f.on( 'application/json' ) { @tasks.to_json }
+#   #     #f.on('*/*') {  Task.all.map{ |t| t.to_s }.to_s }
+#   #     f.on('*/*') { haml :'tasks/index' }
+#   #
+#   # end
  
-  @tasks.to_json
-end
+#   @tasks.to_json
+# end
  
-get '/tasks/:id' do
-  Task.where(id: params['id']).first.to_json
-end
+# get '/tasks/:id' do
+#   Task.where(id: params['id']).first.to_json
+# end
  
-# curl -X POST -F 'name=test' -F 'list_id=1' http://localhost:9393/tasks
+# # curl -X POST -F 'name=test' -F 'list_id=1' http://localhost:9393/tasks
  
-post '/tasks' do
-  task = Task.new(params)
+# post '/tasks' do
+#   task = Task.new(params)
  
-  if task.save
-    task.to_json
-  else
-    halt 422, task.errors.full_messages.to_json
-  end
-end
+#   if task.save
+#     task.to_json
+#   else
+#     halt 422, task.errors.full_messages.to_json
+#   end
+# end
  
-# curl -X PUT -F 'name=updates' -F 'list_id=1' http://localhost:9393/tasks/1
+# # curl -X PUT -F 'name=updates' -F 'list_id=1' http://localhost:9393/tasks/1
  
-put '/tasks/:id' do
-  task = Task.where(id: params['id']).first
+# put '/tasks/:id' do
+#   task = Task.where(id: params['id']).first
  
-  if task
-    task.name = params['name'] if params.has_key?('name')
-    task.is_completed = params['is_completed'] if params.has_key?('is_completed')
+#   if task
+#     task.name = params['name'] if params.has_key?('name')
+#     task.is_completed = params['is_completed'] if params.has_key?('is_completed')
     
-    if task.save
-      task.to_json
-    else
-      halt 422, task.errors.full_messages.to_json
-    end
-  end
-end
+#     if task.save
+#       task.to_json
+#     else
+#       halt 422, task.errors.full_messages.to_json
+#     end
+#   end
+# end
  
-delete '/tasks/:id' do
-  task = Task.where(id: params['id'])
+# delete '/tasks/:id' do
+#   task = Task.where(id: params['id'])
  
-  if task.destroy_all
-    {success: "ok"}.to_json
-  else
-    halt 500
-  end
-end
+#   if task.destroy_all
+#     {success: "ok"}.to_json
+#   else
+#     halt 500
+#   end
+# end
 
-get '/lists' do
-  List.all.to_json(include: :tasks)
-end
+# get '/lists' do
+#   List.all.to_json(include: :tasks)
+# end
  
-get '/lists/:id' do
-  List.where(id: params['id']).first.to_json(include: :tasks)
+# get '/lists/:id' do
+#   List.where(id: params['id']).first.to_json(include: :tasks)
   
-  # my_list = List.where(id: params['id']).first
-  # my_list.tasks
-  #
-  # my_task = Task.where(id: params['id']).first
-  # my_task.list
+#   # my_list = List.where(id: params['id']).first
+#   # my_list.tasks
+#   #
+#   # my_task = Task.where(id: params['id']).first
+#   # my_task.list
   
   
-end
+# end
  
-post '/lists' do
-  list = List.new(params)
+# post '/lists' do
+#   list = List.new(params)
  
-  if list.save
-    list.to_json(include: :tasks)
-  else
-    halt 422, list.errors.full_messages.to_json
-  end
-end
+#   if list.save
+#     list.to_json(include: :tasks)
+#   else
+#     halt 422, list.errors.full_messages.to_json
+#   end
+# end
  
-put '/lists/:id' do
-  list = List.where(id: params['id']).first
+# put '/lists/:id' do
+#   list = List.where(id: params['id']).first
  
-  if list
-    list.name = params['name'] if params.has_key?('name')
+#   if list
+#     list.name = params['name'] if params.has_key?('name')
  
-    if list.save
-      list.to_json
-    else
-      halt 422, list.errors.full_messages.to_json
-    end
-  end
-end
+#     if list.save
+#       list.to_json
+#     else
+#       halt 422, list.errors.full_messages.to_json
+#     end
+#   end
+# end
  
-delete '/lists/:id' do
-  list = List.where(id: params['id'])
+# delete '/lists/:id' do
+#   list = List.where(id: params['id'])
  
-  if list.destroy_all
-    {success: "ok"}.to_json
-  else
-    halt 500
-  end
-end
-# ----------------------------------------------------------------------
-#     ERRORS
-# ----------------------------------------------------------------------
+#   if list.destroy_all
+#     {success: "ok"}.to_json
+#   else
+#     halt 500
+#   end
+# end
+# # ----------------------------------------------------------------------
+# #     ERRORS
+# # ----------------------------------------------------------------------
 
 
-error 401 do 
-  "Why is this happening!!!"
-end
+# error 401 do 
+#   "Why is this happening!!!"
+# end
 
 
-# ----------------------------------------------------------------------
-#   METHODS
-#   Add any custom methods below
-# ----------------------------------------------------------------------
+# # ----------------------------------------------------------------------
+# #   METHODS
+# #   Add any custom methods below
+# # ----------------------------------------------------------------------
 
-private
+# private
 
-# for example 
-def square_of int
-  int * int
-end
+# # for example 
+# def square_of int
+#   int * int
+# end
